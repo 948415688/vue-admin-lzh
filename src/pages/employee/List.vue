7022d34c1e0f4152b5f2010945ea16e033e90b42
948415688 <948415688@qq.com>
2020-01-03 16:47:34 +0800

    add comments

--- src/pages/employee/List.vue
+++ src/pages/employee/List.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- asdkkasjd -->
     <!-- 按钮 -->
     <el-button type="success" size="small" @click="toAddHandler">添加</el-button> 
     <el-button type="danger" size="small">批量删除</el-button>
